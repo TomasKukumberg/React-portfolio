ecde064bf798d8cd948f89782c66437bb8cc11f7
Tomas Kukumberg <tomas.kukumberg@centrum.sk>
2023-07-31 19:04:47 +0200

    remove double attribute

--- src/components/SidenavMdItem.jsx
+++ src/components/SidenavMdItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SidenavMdItem = ({id, icon, text}) => {
     return (
-        <a href={'#' + id} title={text} className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300' title={text}>
+        <a href={'#' + id} title={text} className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
             {icon}
         </a>
     )
