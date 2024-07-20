2cc8687a65a502097f87c3332246e34876387e7b
TomasKukumberg <tomas.kukumberg@centrum.sk>
2024-07-20 23:03:43 +0200

    add keyframe animation for logos

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        scroll: {
+          '0%': {transform: 'translateX(0)'},
+          '100%': {transform: 'translateX(calc(-150px * 5))'}, 
+        }
+      },
+      animation: {
+        'scroll': 'scroll 15s 0.5s linear infinite',
+      }
+    },
   },
   plugins: [],
 }
