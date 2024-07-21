982c62bb3771c635bfd46db7352ba83ccd7c4055
TomasKukumberg <tomas.kukumberg@centrum.sk>
2024-07-21 19:08:30 +0200

    put logo data inside separate file and map it in logos component

--- src/components/Logos.jsx
+++ src/components/Logos.jsx
@@ -1,73 +1,21 @@
-import reactLogo from '../assets/react.svg'
-import vueLogo from '../assets/vue.svg'
-import tsLogo from '../assets/typescript.svg'
-import tailwindLogo from '../assets/tailwind.svg'
-import bootstrapLogo from '../assets/bootstrap.svg'
-import laravelLogo from '../assets/laravel.svg'
-import htmlLogo from '../assets/html.svg'
-import cssLogo from '../assets/css.svg'
-import jsLogo from '../assets/javascript.svg'
-import alpineLogo from '../assets/alpinejs.svg'
-import viteLogo from '../assets/vite.svg'
-import webpackLogo from '../assets/webpack.svg'
-import linuxLogo from '../assets/linux.svg'
-import typoLogo from '../assets/typo3.svg'
-import jiraLogo from '../assets/jira.svg'
+import { logos } from '../data/logos'
 
 
 const Logos = () => {
     return (
-      <div className='mt-8 h-32 overflow-hidden relative w-full flex items-center py-0 px-5 before:content-[""] after:content-[""] before:absolute after:absolute
+        <div className='mt-8 h-32 overflow-hidden relative w-full flex items-center py-0 px-5 before:content-[""] after:content-[""] before:absolute after:absolute
       before:h-[130px] after:h-[130px] before:w-[150px] after:w-[150px] before:z-[2] after:z-[2] before:left-0 before:top-0 after:right-0 after:top-0 after:rotate-180'>
-        <div className='flex w-[150px * 15] animate-scroll'>
-            <div className='w-[150px]'>
-                <img src={reactLogo} alt="react logo" className="h-[90px]" />
-            </div>
-            <div className='w-[150px]'>
-                <img src={vueLogo} alt="vue logo" className="h-[90px]" />
-            </div>
-            <div className='w-[150px]'>
-                <img src={tsLogo} alt="typescript logo" className="h-[90px]" />
-            </div>
-            <div className='w-[150px]'>
-                <img src={tailwindLogo} alt="tailwind logo" className="h-[90px]" />
-            </div>
-            <div className='w-[150px]'>
-                <img src={bootstrapLogo} alt="bootstrap logo" className="h-[90px]" />
-            </div>
-            <div className='w-[150px]'>
-                <img src={laravelLogo} alt="laravel logo" className="h-[90px]" />
-            </div>
-            <div className='w-[150px]'>
-                <img src={htmlLogo} alt="html logo" className="h-[90px]" />
-            </div>
-            <div className='w-[150px]'>
-                <img src={cssLogo} alt="css logo" className="h-[90px]" />
-            </div>
-            <div className='w-[150px]'>
-                <img src={jsLogo} alt="javascript logo" className="h-[90px]" />
-            </div>
-            <div className='w-[150px]'>
-                <img src={alpineLogo} alt="alpine.js logo" className="h-[90px]" />
-            </div>
-            <div className='w-[150px]'>
-                <img src={linuxLogo} alt="linux logo" className="h-[90px]" />
-            </div>
-            <div className='w-[150px]'>
-                <img src={viteLogo} alt="vite logo" className="h-[90px]" />
-            </div>
-            <div className='w-[150px]'>
-                <img src={webpackLogo} alt="webpack logo" className="h-[90px]" />
-            </div>
-            <div className='w-[150px]'>
-                <img src={jiraLogo} alt="jira logo" className="h-[90px]" />
-            </div>
-            <div className='w-[150px]'>
-                <img src={typoLogo} alt="typo3 logo" className="h-[90px]" />
+            <div className='flex w-[150px * 15] animate-scroll'>
+                {
+                    logos.map(logo => (
+                        <div className='w-[150px]'>
+                            <img src={logo.src} alt={`${logo.name} logo`} className="h-[90px]" />
+                        </div>
+                    ))
+                }
             </div>
         </div>
-      </div>
     )
-  }
+}
   
-  export default Logos;
+export default Logos;
---
+++ src/data/logos.js
@@ -0,0 +1,78 @@
+import reactLogo from '../assets/react.svg'
+import vueLogo from '../assets/vue.svg'
+import tsLogo from '../assets/typescript.svg'
+import tailwindLogo from '../assets/tailwind.svg'
+import bootstrapLogo from '../assets/bootstrap.svg'
+import laravelLogo from '../assets/laravel.svg'
+import htmlLogo from '../assets/html.svg'
+import cssLogo from '../assets/css.svg'
+import jsLogo from '../assets/javascript.svg'
+import alpineLogo from '../assets/alpinejs.svg'
+import viteLogo from '../assets/vite.svg'
+import webpackLogo from '../assets/webpack.svg'
+import linuxLogo from '../assets/linux.svg'
+import typoLogo from '../assets/typo3.svg'
+import jiraLogo from '../assets/jira.svg'
+
+export const logos = [
+    {
+        name: 'react',
+        src: reactLogo,
+    },
+    {
+        name: 'vue',
+        src: vueLogo,
+    },
+    {
+        name: 'typescript',
+        src: tsLogo,
+    },
+    {
+        name: 'tailwind',
+        src: tailwindLogo,
+    },
+    {
+        name: 'bootstrap',
+        src: bootstrapLogo,
+    },
+    {
+        name: 'laravel',
+        src: laravelLogo,
+    },
+    {
+        name: 'html',
+        src: htmlLogo,
+    },
+    {
+        name: 'css',
+        src: cssLogo,
+    },
+    {
+        name: 'javascript',
+        src: jsLogo,
+    },
+    {
+        name: 'alpine',
+        src: alpineLogo,
+    },
+    {
+        name: 'vite',
+        src: viteLogo,
+    },
+    {
+        name: 'webpack',
+        src: webpackLogo,
+    },
+    {
+        name: 'typo3',
+        src: typoLogo,
+    },
+    {
+        name: 'linux',
+        src: linuxLogo,
+    },
+    {
+        name: 'jira',
+        src: jiraLogo,
+    },
+];
